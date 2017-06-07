dbf5d87b14d39fcb5aaecb6919c0dfec29109be8
Rachel Buckner <rachelbuckner@gmail.com>
2017-06-06 20:50:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  var quote;
+  var author;
+
+  function getNewQuote() {
+    $.ajax({
+      url:"http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(response) {
+        quote = response.quoteText;
+        author = response.quoteAuthor;
+        $("#quote").text(quote).hide().fadeIn(1500);
+        if (author) {
+          $("#author").text("- " + author).hide().fadeIn(1500);
+        } else {
+          $("#author").text("- Unknown").hide().fadeIn(1500);
+        }
+      }
+    });
+  }
+
+
+getNewQuote();
+
+$("#getQuote").on("click", function(event) {
+  event.preventDefault();
+  getNewQuote();
+});
+
+$("#tweet-quote").on("click", function(event) {
+  event.preventDefault();
+  window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + quote + '"' + ' - ' + author))
+});
+});
